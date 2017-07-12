e0695afd5f213d5c83b21aed5445e78ad8c0d7e0
Sudil Shrestha <shresthasudil@gmail.com>
2017-07-12 10:49:25 -0500

    Done E5: Pass Data to Your Views.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
+// This is a Route with closure function
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Passing data to View
+Route::get('/about', function () {
+    return view('about', [
+        'name' => ''
+    ]);
+});
+
+/**
+ * Pass Data to View
+ * can be passed by second argument like below or
+ * return view('passdata')->with('name', 'World');      // or
+ * if stored as a variable like
+ * $name = 'World';
+ * return view('passdata', compact('name'));            // equivalent to return view('passdata', ['name' => $name]);
+ */
+Route::get('/passdata', function () {
+    $tasks = [
+        'go to the store',
+        'finish my screencas',
+        'clean the house',
+    ];
+
+    return view('passdata', [
+        'name' => 'World',
+        'tasks' => $tasks,
+    ]);
 });
