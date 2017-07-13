b24168f278f430e57f215aeb7d236ab9c427a072
Sudil Shrestha <shresthasudil@gmail.com>
2017-07-13 11:02:52 -0500

    Done E7: Eloquent 101.

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    // regular static function which returns the result of the query.
+    public static function incomplete()
+    {
+        return static::where('completed', 0)->get();
+    }
+
+    // using a query score(wrapper around query), multiple chains can be added into the query.
+    public function scopeIncomplete($query)       // allow us to do: Task::incomplete() same as above with param
+    {
+        return $query->where('completed', 0);
+    }
 }
