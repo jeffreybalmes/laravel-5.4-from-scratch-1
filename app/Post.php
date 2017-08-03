856972e89fb1be3dc6d85cb22e4f75d489743faf
Sudil Shrestha <shresthasudil@gmail.com>
2017-08-03 14:52:30 -0500

    Done E15: Eloquent Relationships and Comments.

--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
 class Post extends Model
 {
-    protected $fillable =['title', 'body']; // fillable will whitelist the field to be submitted
-    //protected $guarded = [];              // inverse of fillable, do not allow this field to be submitted.
+    //protected $fillable =['title', 'body'];   // fillable will whitelist the field to be submitted
+    //protected $guarded = [];                  // inverse of fillable, do not allow this field to be submitted.
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
