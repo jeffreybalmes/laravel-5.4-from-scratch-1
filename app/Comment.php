d14fad1c67afe3ba4f3bf7c31627123c91a91482
Sudil Shrestha <shresthasudil@gmail.com>
2017-08-10 10:09:16 -0500

    Done E18: Associating WIth Users. Skipped E17, its more of configuation and settings and no actual changes in the Blog project we are working on.

--- app/Comment.php
+++ app/Comment.php
@@ -9,4 +9,14 @@ class Comment extends Model
     {
         return $this->belongsTo(Post::class);
     }
+
+    /**
+     * Comment belongs to a User.
+     *  $comment->user->name to get name of the User who created the comment.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -12,6 +12,17 @@ class Post extends Model
         return $this->hasMany(Comment::class);
     }
 
+    /**
+     * Post belongs to User
+     * $post->user->name to get the name of User who created the post.
+     * $comment->post->user to get user of the post in which the comment is associated with.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function addComment($body)
     {
         $this->comments()->create(compact('body'));
